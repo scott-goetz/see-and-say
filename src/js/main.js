c4b40c01d06909e093e5db3ad612f4bce81ab28e
Scott Goetz <scottg@criticalmass.com>
2015-10-29 23:22:42 -0600

    Add creative assets and final audio

--- src/js/main.js
+++ src/js/main.js
@@ -3,60 +3,78 @@ $(function () {
 		'arrowRotationsPerSecond': 1.5,
 		'audioFormat': 'mp3',
 		'audioPath': 'media/audio/',
-		'leverStartRotation': 19.25,
-		'leverMaxRotation': 20,
+		'handleStartRotation': 19.25,
+		'handleMaxRotation': 21.14,
 		'playTime': 7,
+		'failAngle': 180,
+		'failMinCount': 2,
+		'failMaxCount': 5
 	};
 
 	var $body = $('body'),
 		$seeSayEl = $('#see-and-say'),
-		$seeSayLeverEl = $seeSayEl.find('.lever'),
-		$seeSayHandleEl = $seeSayLeverEl.find('.handle'),
+		$seeSayHandleEl = $seeSayEl.find('.handle'),
 		$seeSayBaseEl = $seeSayEl.find('.base'),
-		$seeSayArrowEl = $seeSayBaseEl.find('.arrow'),
-		$seeSaySoundsEl = $seeSayBaseEl.find('.sound-base'),
-		$seeSayPlayback = $seeSayEl.find('audio.playback');
+		$seeSayArrowEl = $seeSayEl.find('.arrow'),
+		$seeSaySoundsEl = $seeSayEl.find('.sound-ring'),
+		$seeSayPlayback = $seeSayEl.find('audio.playback'),
+		$seeSayEngineFailure = $seeSayEl.find('audio.engine-failure');
 
 	var arrowRotationAmount,
 		audioDuration,
-		leverOrigAngle,
+		handleOrigAngle,
 		resetDuration,
-		divisionDegrees;
+		divisionDegrees,
+		engineFailure;
+
+	var pullCount = 0;
 
 	var isReady = false;
 
-	// var soundsArr = ['Yoda', 'Admiral Ackbar', 'Darth Vader', 'Lightsaber', 'Tie Fighter', 'R2D2', 'Jabba The Hut', 'Princess Leia', 'Cantina Band', 'Han Solo'];
+	var soundsArr = ['yoda', 'admiral-ackbar', 'lord-vader', 'lightsaber', 'tie-fighter', 'r2d2', 'jabba-the-hut', 'princess-leia', 'cantina-band', 'han-solo'];
 
-	var soundsArr = ['goldblum-1', 'goldblum-2', 'goldblum-3', 'goldblum-4', 'goldblum-5', 'goldblum-6', 'goldblum-7', 'goldblum-8', 'goldblum-9', 'goldblum-10'];
+	// var soundsArr = ['goldblum-1', 'goldblum-2', 'goldblum-3', 'goldblum-4', 'goldblum-5', 'goldblum-6', 'goldblum-7', 'goldblum-8', 'goldblum-9', 'goldblum-10'];
 
 	var arrowDraggableParams = {
 		rotationCenterX: 50.0,
 		rotationCenterY: 50.0
 	};
 
-	var leverDraggableParams = {
-		angle: options.leverStartRotation,
+	var handleDraggableParams = {
+		angle: options.handleStartRotation,
 		start: function(event, ui) {
 			if (!isReady) {
 				isReady = true;
 			}
 
-			// Capture original lever angle
-			leverOrigAngle = getRotation($seeSayLeverEl);
+			// Capture original handle angle
+			handleOrigAngle = getRotation($seeSayHandleEl);
 		},
 		rotate: function(event, ui) {
-			// Stop the lever at a certain point
-			if (ui.angle.current >= options.leverMaxRotation) {
+			// Stop the handle at a certain point
+			if (ui.angle.current >= options.handleMaxRotation) {
 				$(this).trigger('mouseup');
 			}
+
+			// console.log(ui.angle.current);
 		},
 		stop: function(event, ui) {
-			// var timeDiff = Math.floor(((ui.angle.current - options.leverStartRotation) / (options.leverMaxRotation - options.leverStartRotation)) * 100) / 100;
+			// var timeDiff = Math.floor(((ui.angle.current - options.handleStartRotation) / (options.handleMaxRotation - options.handleStartRotation)) * 100) / 100;
 
 			// resetDuration = options.playTime * timeDiff;
 
-			// Change new audio
-			audioUpdate(Math.floor(getRotation($seeSayArrowEl) / divisionDegrees));
+			// Update pull count
+			pullCount++;
+
+			// Engine failure or sound effect
+			if (pullCount == engineFailureCount) {
+				engineFailure();
+			} else {
+				// Change new audio
+				audioUpdate(Math.floor(getRotation($seeSayArrowEl) / divisionDegrees));
+			}
+
+			
 		},
 		rotationCenterX: 50.0,
 		rotationCenterY: 100.0
@@ -69,6 +87,10 @@ $(function () {
 		// Set some variables
 		divisionDegrees = 360 / soundsArr.length;
 
+		// Determine random engine failure
+		engineFailureCount = setEngineFailure(options.failMinCount, options.failMaxCount);
+		// engineFailureCount = 1;
+
 		attach();		 
 	};
 
@@ -77,7 +99,7 @@ $(function () {
 	 */
 	function attach () {
 		// Attach rotatable
-		$seeSayLeverEl.draggable({ handle: '.ui-rotatable-handle' }).rotatable(leverDraggableParams);
+		$seeSayHandleEl.draggable({ handle: '.ui-rotatable-handle' }).rotatable(handleDraggableParams);
 		$seeSayArrowEl.draggable({ handle: '.ui-rotatable-handle' }).rotatable(arrowDraggableParams);
 
 		// Audio element listeners
@@ -91,10 +113,40 @@ $(function () {
 				$seeSayPlayback[0].play();
 
 				// Lever reset and arrow rotation
-				resetLever(resetDuration);
-				rotateArrow(arrowRotationAmount);
+				resetHandle(audioDuration);
+
+				rotateArrow(arrowRotationAmount, audioDuration);
 			}
 		});
+	}
+
+	/**
+	 * Play the audio of whichever character/object the arrow is currently pointing at.
+	 * @param  {Int} soundIndex Index of the appropriate sound effect.
+	 */
+	function audioUpdate (soundIndex) {
+		var audioSrc = options.audioPath + soundsArr[soundIndex] + '.' + options.audioFormat;
+
+		// Swap audio source and play
+		$seeSayPlayback.attr('src', audioSrc);
+	}
+
+	/**
+	 * Play engine failure sound and animation.
+	 */
+	function engineFailure () {
+
+		// Update audio duration and determine arrow spin time
+		audioDuration = $seeSayPlayback[0].duration;
+		arrowRotationAmount = (audioDuration * options.arrowRotationsPerSecond) * 360;
+
+		// Reset for next engine failure
+		pullCount = 0;
+		engineFailureCount = setEngineFailure(options.failMinCount, options.failMaxCount);	
+
+		$seeSayEngineFailure[0].play();
+		resetHandle(1);
+		rotateArrow(arrowRotationAmount, (audioDuration - 2.5));
 	}
 
 	/**
@@ -120,32 +172,22 @@ $(function () {
 	}
 
 	/**
-	 * Play the audio of whichever character/object the arrow is currently pointing at.
+	 * Reset the handle position over time.
 	 */
-	function audioUpdate (soundIndex) {
-		var audioSrc = options.audioPath + soundsArr[soundIndex] + '.' + options.audioFormat;
-
-		// Swap audio source and play
-		$seeSayPlayback.attr('src', audioSrc);
-	}
-
-	/**
-	 * Reset the lever position over time.
-	 */
-	function resetLever () {
+	function resetHandle (duration) {
 		// Disable rotatable functionality temporarily
-		$seeSayLeverEl.rotatable('destroy');
+		$seeSayHandleEl.rotatable('destroy');
 
-		// Animate lever back into position
-		TweenMax.fromTo($seeSayLeverEl, audioDuration, {
-				rotation: getRotation($seeSayLeverEl)
+		// Animate handle back into position
+		TweenMax.fromTo($seeSayHandleEl, duration, {
+				rotation: getRotation($seeSayHandleEl)
 			},
 			{
-				rotation: leverOrigAngle,
+				rotation: handleOrigAngle,
 				// ease: Power3.easeOut
 				onComplete: function () {
 					// Reset rotatable
-					$seeSayLeverEl.removeAttr('style').rotatable(leverDraggableParams);
+					$seeSayHandleEl.removeAttr('style').rotatable(handleDraggableParams);
 				}
 			}
 		);
@@ -155,13 +197,13 @@ $(function () {
 	 * Spin the arrow to it's new position.
 	 * @param  {Int} degrees  The amount, in degrees, to spin the arrow.
 	 */
-	function rotateArrow (degrees) {
+	function rotateArrow (degrees, duration) {
 		// Disable rotatable functionality temporarily
 		$seeSayArrowEl.rotatable('destroy');
 
 		var newArrowDraggableParams = arrowDraggableParams;
 
-		TweenMax.to($seeSayArrowEl, audioDuration, {
+		TweenMax.to($seeSayArrowEl, duration, {
 			css: { rotation: '+= ' + degrees + '_cw' },
 			// ease: Power3.easeOut
 			onComplete: function () {
@@ -172,6 +214,10 @@ $(function () {
 				$seeSayArrowEl.removeAttr('style').rotatable(newArrowDraggableParams);
 			}
 		});
+	}
+
+	function setEngineFailure (min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
 
 	init();
